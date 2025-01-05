df9b6e75cd547b4bf59ddacf76942e15eb322a00
sanjay bhanadadre <sanjayvb1111@gmail.com>
2025-01-05 17:24:31 +0530

    added some chamges

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,68 +1,126 @@
-import React from 'react'
-import images from '../utils/images'
-
+import React, { useState } from 'react';
+import images from '../utils/images';
+import "./styles.css";
+import Masonry from 'react-masonry-css';
+// 1W35vdhHfnA2_t9bi8BZjK2tXTVr8OKYE
 const list = [
     {
         name: 'Bird',
         img: images.one,
-        desc: 'bird is good'
+        desc: 'Bird is good',
+    },
+    {
+        name: 'Bird 13',
+        img: images.thirteen,
+        desc: 'Bird is good',
     },
     {
         name: 'Bird 02',
         img: images.two,
-        desc: 'bird is good'
+        desc: 'Bird is good',
+    },
+    {
+        name: 'Bird 12',
+        img: images.twelve,
+        desc: 'Bird is good',
     },
     {
         name: 'Bird 03',
         img: images.three,
-        desc: 'bird is good'
+        desc: 'Bird is good',
     },
     {
         name: 'Bird 04',
         img: images.four,
-        desc: 'bird is good'
+        desc: 'Bird is good',
     },
     {
         name: 'Bird 05',
         img: images.five,
-        desc: 'bird is good'
+        desc: 'Bird is good',
     },
     {
         name: 'Bird 06',
         img: images.six,
-        desc: 'bird is good'
+        desc: 'Bird is good',
+    },
+    {
+        name: 'Bird 14',
+        img: images.fourteen,
+        desc: 'Bird is good',
     },
     {
         name: 'Bird 07',
         img: images.seven,
-        desc: 'bird is good'
+        desc: 'Bird is good',
     },
     {
         name: 'Bird 08',
         img: images.eight,
-        desc: 'bird is good'
-    }
-]
+        desc: 'Bird is good',
+    },
+    {
+        name: 'Bird 09',
+        img: images.nine,
+        desc: 'Bird is good',
+    },
+    {
+        name: 'Bird 11',
+        img: images.eleven,
+        desc: 'Bird is good',
+    },
+
+    {
+        name: 'Bird 10',
+        img: images.ten,
+        desc: 'Bird is good',
+    },
+   
+];
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height
+    };
+  }
 
 const HomeScreen = () => {
+
+     const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+    const breakpointColumnsObj = {
+        default: 4, // 3 columns for large screens
+        1100: 2,    // 2 columns for medium screens
+        700: 1,     // 1 column for small screens
+    };
+
     return (
-        <div style={{backgroundColor: '#fff'}}>
-            <div style={{padding: 40 , display: 'flex', alignItems:'center', justifyContent: 'space-between', flexDirection: 'row', flexWrap: 'wrap'}}>
+        <div style={{ backgroundColor: '#fff', padding: '40px' }}>
+            <div style={{height: windowDimensions.height, alignItems: 'center', justifyContent: 'center',}}>
+                <div></div>
+                <div style={{fontSize: 100, fontWeight: 'bold'}}>
+                    Ajit Bhanadre
+                </div>
+                <div style={{fontSize: 100, fontWeight: 'bold'}}>
+                Wildlife Photography
+                </div>
+            </div>
 
-          {
-              list.map((item)=> {
-                  return (
-                      <div>
-                        <img style={{width: 600}} src={item.img} />
-                        <div>Bird Name - {item.name}</div>
-                        <div> Desc - {item.desc} </div>
+            <Masonry
+                breakpointCols={breakpointColumnsObj}
+                className="masonry-grid"
+                columnClassName="masonry-column"
+            >
+                {list.map((item, index) => (
+                    <div key={index} className="masonry-item">
+                        <img src={item.img} alt={item.name} style={{ width: '100%', height: 'auto', borderRadius: '8px' }} />
                     </div>
-                )
-            })
-        }
+                ))}
+            </Masonry>
         </div>
-        </div>
-    )
-}
+    );
+};
 
-export default HomeScreen
+
+export default HomeScreen;
--- src/utils/images.js
+++ src/utils/images.js
@@ -6,6 +6,12 @@ import five from '../Assets/img/05.jpg'
 import six from '../Assets/img/06.jpg'
 import seven from '../Assets/img/07.jpg'
 import eight from '../Assets/img/08.jpg'
+import nine from '../Assets/img/09.jpg'
+import ten from '../Assets/img/10.jpg'
+import eleven from '../Assets/img/11.jpg'
+import twelve from '../Assets/img/12.jpg'
+import thirteen from '../Assets/img/13.jpg'
+import fourteen from '../Assets/img/14.jpg'
 
 export default {
     one,
@@ -15,5 +21,11 @@ export default {
     five,
     six,
     seven,
-    eight
+    eight,
+    nine,
+    ten,
+    eleven,
+    twelve,
+    thirteen,
+    fourteen
 }
